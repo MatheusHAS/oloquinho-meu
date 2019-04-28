a7536d1e0a76569afe1dcbdb35f47486f295b8a6
Matheus Azambuja <matheushenrique.ads@gmail.com>
2019-04-27 22:23:05 -0300

    🎤 Oloquinho meu

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # oloquinho-meu
+> "Baseado na obra de [@haskellcamargo, gemidao-do-zap](https://github.com/haskellcamargo/gemidao-do-zap)"
+
+# O que isso faz?
+Envie o famoso "Oloquinho meu" para seus amigos via chamada telefonica.
+
+É necessário criar uma conta no [TotalVoice](https://totalvoice.com.br), ele te fornecem R$ 3,00 de créditos testar o serviço. Cada envio de "oloquinhos", custa cerca de R$ 0,35. Use com sabedoria.
+
+Para se cadastrar é necessário um e-mail e um celular válido. O Celular que você cadastra na sua conta, será sua "bina", portanto você deverá usar esse número no parametro `--de` (mais informações abaixo). 
+
+Caso deseje cadastrar mais números como "bina" é possível através do painel administrativo do TotalVoice.
+
+Na tela inicial após logado no TotalVoice você pode obter seu *Token*
+
+# Do que preciso?
+`node`
+
+# Instalação
+`sudo npm install`
+
+# E para executar?
+* `node index.js --de {telefone} --para {telefone} --token {tokenTotalVoice}`
+
+ou simplesmente
+
+* `node index.js`
+E informar os dados conforme solicitados.
+
+| Parâmetro  |  Obrigatório  | Descrição |
+| :------------ |:---------------:| -----:|
+| --de | ✅ | Número de telefone da Bina (cadastrado no site do TotalVoice) |
+| --para | ✅ | Número de telefone da pessoa que receberá |
+| --token | ✅ | Seu token de acesso do TotalVoice |
+
+# Disclaimer
+Os autores deste projeto não possuem qualquer ligação com a TotalVoice. TotalVoice é uma marca registrada.
---
+++ index.js
@@ -0,0 +1,57 @@
+const axios = require('axios')
+let argv = require('minimist')(process.argv.slice(2))
+const stdio = require('stdio');
+
+const OPTIONS = {
+    ENDPOINT: `https://api2.totalvoice.com.br`,
+}
+
+const oloquinho = {
+    meu: async (from, to, token) => {
+        try {
+            to = to.replace(/[()\- a-zA-Z]/g, to)
+            from = from.replace(/[()\- a-zA-Z]/g, from)
+
+            console.log(`ô loquinho meu...`)
+            console.log(`De: ${from}\nPara: ${to}\nToken: ${token}`)
+
+            const axiosInstance = axios.create({
+                baseURL: OPTIONS.ENDPOINT,
+                timeout: 5000,
+                headers: {
+                    'Access-Token': token,
+                }
+            })
+
+            await axiosInstance.post('/composto', {
+                    numero_destino: `${to}`,
+                    dados: [
+                        {
+                            acao: 'audio',
+                            acao_dados: {
+                                url_audio: 'https://github.com/MatheusHAS/oloquinho-meu/raw/master/oloquinho-meu.mp3',
+                            },
+                        }
+                    ],
+                    bina: from
+                })
+                .then((resp) => console.log(`Essa ferinha ai meu, quem sabe faz ao vivo!`))
+                .catch((err) => console.error(`Tá pegando fogo bixo: ${err}`))
+            
+        } catch (exception) {
+            console.error(`Eita: ${exception}`)
+        }
+    }
+}
+
+if (argv.de && argv.para && argv.token) {
+    oloquinho.meu(argv.de, argv.para, argv.token)
+} else {
+    stdio.question('Destinatário: Digite o Telefone (com DDD)', function (err, to) {
+        stdio.question('Remetente: Digite o Telefone (com DDD)', function (err, from) {
+            stdio.question('Token do TotalVoice', function (err, token) {
+                oloquinho.meu(from, to, token)
+            });
+        });
+    });
+}
